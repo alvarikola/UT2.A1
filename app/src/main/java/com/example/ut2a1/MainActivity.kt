5d4ecd2868ebbb1c15b02cc8c6386d7c1027bd99
alvarikola <alvaroe484@gmail.com>
2024-10-02 09:26:38 +0100

    Actualizado

--- app/src/main/java/com/example/ut2a1/MainActivity.kt
+++ app/src/main/java/com/example/ut2a1/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.ComponentActivity
 //https://developer.android.com/reference/androidx/activity/ComponentActivity
 import android.os.Environment
 //https://developer.android.com/reference/android/os/Environment
-import android.os.Message
-//https://developer.android.com/reference/android/os/Environment
 import android.util.Log
 //https://developer.android.com/reference/android/util/Log
 import java.io.BufferedReader
@@ -16,12 +14,8 @@ import java.io.File
 //https://developer.android.com/reference/java/io/File
 import java.io.FileOutputStream
 //https://developer.android.com/reference/java/io/FileOutputStream
-import java.io.FileInputStream
-//https://developer.android.com/reference/java/io/FileInputStream
 import java.io.FileReader
 //https://developer.android.com/reference/java/io/FileReader
-import java.io.InputStreamReader
-//https://developer.android.com/reference/java/io/InputStreamReader
 import java.io.OutputStreamWriter
 //https://developer.android.com/reference/java/io/OutputStreamWriter
 
@@ -55,11 +49,12 @@ class MainActivity : ComponentActivity() {
             try {
                 val flujoEntrada = FileReader(archivo)
                 val leer = BufferedReader(flujoEntrada)
-                val contenidoArchivo = leer.readLines().joinToString(" ")
+                val contenidoArchivo = leer.readLines()
                 leer.close()
 
                 Log.i("DAM2", "Contenido del archivo: $contenidoArchivo")
                 flujoEntrada.close()
+                // File(directorio, nombreArchivo).delete() esto es para borrar y que no se acumulen los datos
             } catch (e: Exception) {
                 e.printStackTrace()
                 Log.i("DAM2", "Error al leer")
